56480580b8f497335f9cbbe6a6efd63e2ea5238c
sures <sures@LAPTOP-4M6LAJK0>
2021-05-10 11:23:49 -0400

    practice2

---
+++ src/practiceonrediff/practice2.java
@@ -0,0 +1,11 @@
+package practiceonrediff;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my practice2");
+
+	}
+
+}
