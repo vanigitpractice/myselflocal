2749f35d4dcbd92445513e53148443c799528bc8
sures <sures@LAPTOP-4M6LAJK0>
2021-05-10 11:26:10 -0400

    package2

---
+++ src/mysecondpackage/package2.java
@@ -0,0 +1,11 @@
+package mysecondpackage;
+
+public class package2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("practice on package");
+
+	}
+
+}
