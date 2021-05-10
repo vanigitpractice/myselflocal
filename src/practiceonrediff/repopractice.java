415bf8865ef55a711d4e0e80ebb23f71f9b0a16e
sures <sures@LAPTOP-4M6LAJK0>
2021-05-10 11:20:09 -0400

    my self

---
+++ src/practiceonrediff/repopractice.java
@@ -0,0 +1,11 @@
+package practiceonrediff;
+
+public class repopractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is practice my self");
+
+	}
+
+}
